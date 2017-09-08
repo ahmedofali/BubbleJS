813132634985dd68f2027584694dfed1753da42b
ahmedofali <dev.ahmed.abbas@gmail.com>
2017-09-08 23:39:25 +0200

    adding size and number of bubbles from configuration

--- BubbleJS.js
+++ BubbleJS.js
@@ -41,8 +41,9 @@ window.BubblerJS = function(tag_id, params){
         canvas_el.className = this.canvas_class;
 
         /* set size canvas */
-        canvas_el.style.width = "100%";
-        canvas_el.style.height = "100%";
+        var BJS_tag = document.getElementById( this.tag_id );
+        canvas_el.width = BJS_tag.clientWidth ;
+        canvas_el.height = BJS_tag.clientHeight;
 
         return document.getElementById(this.tag_id).appendChild(canvas_el);
     };
@@ -62,10 +63,21 @@ window.BubblerJS = function(tag_id, params){
 
 var BJS = function( tag_id, settings ){
 
+    /**
+     * User Submitted Settings
+     */
     this.settings = settings ;
 
+    /**
+     * Finall Settings after merging user | default settings
+     * @type {null}
+     */
     this.final_configuration = null ;
 
+    /**
+     * get default settings
+     * @returns {{settings: {number: {value: number}, color: {value: string}, opacity: {value: number, random: boolean, anim: {enable: boolean, speed: number, opacity_min: number}}, size: {value: number, random: boolean, anim: {enable: boolean, speed: number, size_min: number}}, line_linked: {enable: boolean, distance: number, color: string, opacity: number, width: number}, move: {enable: boolean, speed: number}}}}
+     */
     this.get_default_settings = function() {
         return {
             "settings": {
@@ -73,7 +85,7 @@ var BJS = function( tag_id, settings ){
                     "value": 33,
                 },
                 "color": {
-                    "value": [ "#225378" , "#1695A3" , "#ACF0F2" , "#F3FFE2" , "#EB7F00" ]
+                    "value": "#ffffff"
                 },
                 "opacity": {
                     "value": 0.06313181133058181,
@@ -108,23 +120,93 @@ var BJS = function( tag_id, settings ){
         };
     }
 
+    /**
+     * Inject Helpers
+     * @type {Helpers}
+     */
     this.Helpers = new Helpers();
 
-    this.init = function (){
+    /**
+     * Created Bubbles
+     * @type {Array}
+     */
+    this.bubbles = [] ;
 
-    }
+    this.get_color = function(){
+
+        // if user pass object
+        if( typeof( BJS.canvas.colors ) == "object" )
+        {
+            return BJS.canvas.colors[ Math.floor( Math.random() * this.final_configuration.settings.color.value.length ) ] ;
+        }
+        // if user did not pass string return default one
+        else if( typeof( BJS.canvas.colors ) !== "string" )
+        {
+            return this.get_default_settings().settings.color.value ;
+        }
+
+        return BJS.canvas.colors ;
+    };
+
+    this.init = function (){
+        // reset bubbles called start || resize
+        this.bubbles = [] ;
+
+        for( var i = 0 ; i < this.final_configuration.settings.number.value ; i++ )
+        {
+            var radius = this.get_size();
+            var x = Math.random() * ( BJS.canvas.el.width - radius * 2 ) + radius ;
+            var y = Math.random() * ( BJS.canvas.el.height - radius * 2 ) + radius ;
+            var dx = ( Math.random() - 0.5 ) * 4 ;
+            var dy = ( Math.random() - 0.5 ) * 4 ;
+
+            this.bubbles.push( new Bubble( x, y, 5, 5, radius , 20, this.get_color(), .5) ) ;
+        }
+    };
 
     this.addEventListeners = function (){
 
-    }
+    };
 
     this.animationManager = function(){
+        requestAnimationFrame( this.animationManager.bind( this ) );
 
-    }
+        BJS.canvas.context.clearRect( 0 , 0 , BJS.canvas.el.width , BJS.canvas.el.height );
+
+        this.bubbles.forEach( function( bubble ){
+            bubble.update();
+        });
+    };
+
+
+    this.set_size = function ( size ){
+        if ( size.random == true ){
+            BJS.canvas.size = null ;
+        }else{
+            BJS.canvas.size = size.value ;
+        }
+    };
+
+    this.get_size = function(){
+        if( BJS.canvas.size == null ){
+            return ( Math.random() * 20 ) + 1  ;
+        }
+        return BJS.canvas.size ;
+    };
 
     this.start = function(){
         // merge user config with default config
         this.final_configuration = this.Helpers.deepExtend( this.get_default_settings() , this.settings ) ;
+
+        BJS.canvas = {};
+
+        BJS.canvas.el = document.querySelector('#'+tag_id+' > .bubbles-js-canvas-el');
+
+        BJS.canvas.context = BJS.canvas.el.getContext("2d");
+
+        BJS.canvas.colors = this.final_configuration.settings.color.value ;
+
+        this.set_size( this.final_configuration.settings.size );
 
         // add interactivity
         this.addEventListeners();
@@ -134,13 +216,23 @@ var BJS = function( tag_id, settings ){
 
         // create animation recursive loop
         this.animationManager();
-    }
+    };
 
     // start Bubblerjs
     this.start();
-}
+};
 
-
+/**
+ * @param x
+ * @param y
+ * @param dx
+ * @param dy
+ * @param radius
+ * @param minRadius
+ * @param color
+ * @param opacity
+ * @constructor
+ */
 var Bubble = function( x, y, dx, dy, radius, minRadius, color, opacity ){
     this.x = x ;
     this.y = y ;
@@ -152,16 +244,20 @@ var Bubble = function( x, y, dx, dy, radius, minRadius, color, opacity ){
     this.opacity = opacity ;
 
 
-}
+};
 
 // for memory problems we need this function to be shared between all instances of the class Bubble
 Bubble.prototype.draw = function(){
-    
-}
+    BJS.canvas.context.beginPath();
+    BJS.canvas.context.arc( this.x , this.y ,this.radius, 0 ,Math.PI * 2 ,false ) ;
+    BJS.canvas.context.fillStyle = this.color ;
+    BJS.canvas.context.fill();
+};
 
 Bubble.prototype.update = function(){
 
-}
+    this.draw();
+};
 
 
 var Helpers = function(){
@@ -182,4 +278,4 @@ var Helpers = function(){
         return destination;
     };
 
-}
+};
