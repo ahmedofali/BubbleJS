1366f6350daf6ab0557c8113af45a77570787b88
ahmedofali <dev.ahmed.abbas@gmail.com>
2017-09-08 18:14:09 +0200

    adding structure of the bubbles and it's controller

--- BubbleJS.js
+++ BubbleJS.js
@@ -62,4 +62,124 @@ window.BubblerJS = function(tag_id, params){
 
 var BJS = function( tag_id, settings ){
 
+    this.settings = settings ;
+
+    this.final_configuration = null ;
+
+    this.get_default_settings = function() {
+        return {
+            "settings": {
+                "number": {
+                    "value": 33,
+                },
+                "color": {
+                    "value": [ "#225378" , "#1695A3" , "#ACF0F2" , "#F3FFE2" , "#EB7F00" ]
+                },
+                "opacity": {
+                    "value": 0.06313181133058181,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1
+                    }
+                },
+                "size": {
+                    "value": 11.83721462448409,
+                    "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 40,
+                        "size_min": 0.1
+                    }
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": "#ffffff",
+                    "opacity": 0.4,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 6
+                }
+            }
+        };
+    }
+
+    this.Helpers = new Helpers();
+
+    this.init = function (){
+
+    }
+
+    this.addEventListeners = function (){
+
+    }
+
+    this.animationManager = function(){
+
+    }
+
+    this.start = function(){
+        // merge user config with default config
+        this.final_configuration = this.Helpers.deepExtend( this.get_default_settings() , this.settings ) ;
+
+        // add interactivity
+        this.addEventListeners();
+
+        // create bubbles instances
+        this.init();
+
+        // create animation recursive loop
+        this.animationManager();
+    }
+
+    // start Bubblerjs
+    this.start();
+}
+
+
+var Bubble = function( x, y, dx, dy, radius, minRadius, color, opacity ){
+    this.x = x ;
+    this.y = y ;
+    this.dx = dx ;
+    this.dy = dy ;
+    this.radius = radius ;
+    this.minRadius = minRadius ;
+    this.color = color ;
+    this.opacity = opacity ;
+
+
+}
+
+// for memory problems we need this function to be shared between all instances of the class Bubble
+Bubble.prototype.draw = function(){
+    
+}
+
+Bubble.prototype.update = function(){
+
+}
+
+
+var Helpers = function(){
+
+    this.deepExtend = function(destination, source) {
+        for (var property in source) {
+            // check if the source property is object
+            if ( source[property] && source[property].constructor &&  source[property].constructor === Object )
+            {
+                destination[property] = destination[property] || {};
+                arguments.callee(destination[property], source[property]);
+            }
+            else
+            {
+                destination[property] = source[property];
+            }
+        }
+        return destination;
+    };
+
 }
