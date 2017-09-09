9790ae508ebf7c1154988acad503e9061df0f00c
ahmedofali <dev.ahmed.abbas@gmail.com>
2017-09-09 22:14:03 +0200

    adding site first section and styles

---
+++ docs/js/main.js
@@ -0,0 +1,27 @@
+BubblerJS("hero", {
+    "settings": {
+        "number": {
+            "value": 100,
+        },
+        "color": {
+            "value": [ "#225378" , "#1695A3" , "#ACF0F2" , "#F3FFE2" , "#EB7F00" ,"#3B4A4F" ,"#5D85A8","#5F94C6"]
+        },
+        "size": {
+            "value": 11.83721462448409,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": .1,
+                "size_min": 5 ,
+                "size_max": 15,
+            }
+        },
+        "move": {
+            "enable": true,
+            "speed_min": .2,
+            "speed_max": 2
+        }
+    }});
+
+
+
